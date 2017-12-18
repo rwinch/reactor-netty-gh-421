c6507c76634ce5c1e3fe18789b535cc14bfe4f1f
Rob Winch <rwinch@users.noreply.github.com>
2017-12-18 16:51:53 -0600

    Not Thymeleaf

--- src/main/java/sample/IndexController.java
+++ src/main/java/sample/IndexController.java
@@ -18,6 +18,8 @@ package sample;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Rob Winch
@@ -32,7 +34,44 @@ public class IndexController {
 	}
 
 	@GetMapping("/login")
+	@ResponseBody
 	public String login() {
-		return "login";
+		return login(false);
+	}
+
+	@GetMapping(path="/login",params = "error")
+	@ResponseBody
+	public String loginError() {
+		return login(true);
+	}
+
+	private  String login(boolean error) {
+		return "<!DOCTYPE html>\n" + "<html lang=\"en\">\n" + "<head>\n"
+				+ "\t<meta charset=\"utf-8\">\n"
+				+ "\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n"
+				+ "\t<meta name=\"description\" content=\"\">\n"
+				+ "\t<meta name=\"author\" content=\"\">\n"
+				+ "\t<title>Please Log In</title>\n"
+				+ "\t<link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M\" crossorigin=\"anonymous\">\n"
+				+ "\t<link href=\"http://getbootstrap.com/docs/4.0/examples/signin/signin.css\" rel=\"stylesheet\" crossorigin=\"anonymous\"/>\n"
+				+ "</head>\n" + "<body>\n" + "<div class=\"container\">\n"
+				+ "\t<form class=\"form-signin\" method=\"post\" action=\"/login\">\n"
+				+ "\t\t<h2 class=\"form-signin-heading\">Please Log In</h2>\n"
+				+ error(error)
+				+ "\t\t\t<label for=\"username\" class=\"sr-only\">Username</label>\n"
+				+ "\t\t\t<input type=\"text\" id=\"username\" name=\"username\" class=\"form-control\" placeholder=\"Username\" required autofocus>\n"
+				+ "\t\t</p>\n" + "\t\t<p>\n"
+				+ "\t\t\t<label for=\"password\" class=\"sr-only\">Password</label>\n"
+				+ "\t\t\t<input type=\"password\" id=\"password\" name=\"password\" class=\"form-control\" placeholder=\"Password\" required>\n"
+				+ "\t\t</p>\n"
+				+ "\t\t<button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Sign in</button>\n"
+				+ "\t</form>\n" + "</div>\n" + "</body>\n" + "</html>";
+	}
+
+	private String error(boolean error) {
+		if(error) {
+			return "\t\t<div class=\"alert alert-danger\" role=\"alert\">Invalid\n" + "\t\t\tusername and password.</div>\n";
+		}
+		return "";
 	}
 }
